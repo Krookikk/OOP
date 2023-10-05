2be26206c3da0487cfc0afa188b87a3b59e4b350
Krookikk <s.yakutina@g.nsu.ru>
2023-10-05 14:13:45 +0700

    cl

--- Task_1_1_1/src/main/java/org/example/Main.java
+++ Task_1_1_1/src/main/java/org/example/Main.java
@@ -2,10 +2,10 @@ package org.example;
 
 public class Main {
 
-    public static void main(String[] args) {
-        int[] list = {5, 4, 3, 2, 1};
+    public static void main(final String[] args) {
+        final int[] list = {5, 4, 3, 2, 1};
         int[] mas = Heapsort.heapsort(list);
-        for (int i = 0; i < mas.length; i++){
+        for (int i = 0; i < mas.length; i++) {
             System.out.println(mas[i]);
         }
     }

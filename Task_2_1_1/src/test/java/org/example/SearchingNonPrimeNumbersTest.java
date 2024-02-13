d9485d7f2a1b6b983689c9f48e0915611ba1f952
Svetlana Yakutina <s.yakutina@g.nsu.ru>
2024-02-14 00:24:54 +0700

    commit

--- Task_2_1_1/src/test/java/org/example/SearchingNonPrimeNumbersTest.java
+++ Task_2_1_1/src/test/java/org/example/SearchingNonPrimeNumbersTest.java
@@ -2,13 +2,39 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SearchingNonPrimeNumbersTest {
     @Test
-    public void test() {
+    public void test_mini() {
         int[] numbers = {2, 3, 4};
         boolean a1 = SearchingNonPrimeNumbers.hasNonPrimeSequential(numbers);
         assertTrue(a1);
     }
+
+    @Test
+    public void test_hasNonPrimeSequential() {
+        int[] numbers = {20319251, 6997901, 6997927, 6997937, 17858849, 6997967,
+                6998009, 6998029, 6998039, 20165149, 6998051, 6998053};
+        boolean a1 = SearchingNonPrimeNumbers.hasNonPrimeSequential(numbers);
+        assertFalse(a1);
+    }
+
+    @Test
+    public void test_hasNonPrimeParallel() {
+        int[] numbers = {20319251, 6997901, 6997927, 6997937, 17858849, 6997967,
+                6998009, 6998029, 6998039, 20165149, 6998051, 6998053};
+        boolean a1 = SearchingNonPrimeNumbers.hasNonPrimeParallel(numbers, 4);
+        assertFalse(a1);
+    }
+
+    @Test
+    public void test_hasNonPrimeParallelStream() {
+        int[] numbers = {20319251, 6997901, 6997927, 6997937, 17858849, 6997967,
+                6998009, 6998029, 6998039, 20165149, 6998051, 6998053};
+        boolean a1 = SearchingNonPrimeNumbers.hasNonPrimeParallelStream(numbers);
+        assertFalse(a1);
+    }
+
 }

04b0534f4e0724aabdbf00a2769878ac79e4ebb1
Svetlana Yakutina <s.yakutina@g.nsu.ru>
2024-04-17 11:15:11 +0700

    commit

--- Task_2_2_1/src/test/java/org/example/BakerTest.java
+++ Task_2_2_1/src/test/java/org/example/BakerTest.java
@@ -5,6 +5,8 @@ import org.example.typeOrder.Order;
 import org.example.worker.Baker;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BakerTest {
@@ -23,5 +25,37 @@ public class BakerTest {
         assertEquals(queueOrder.size(), 0);
         assertEquals(queueWarehouse.size(), 1);
     }
+
+    @Test
+    public void test_baker2() throws InterruptedException {
+        MyBlockingQueue queueOrder = new MyBlockingQueue();
+        MyBlockingQueue queueWarehouse = new MyBlockingQueue(10);
+        queueOrder.add(new Order(1, 0, 0));
+        queueOrder.add(new Order(2, 0, 0));
+        queueOrder.add(new Order(3, 0, 0));
+        queueOrder.add(new Order(4, 0, 0));
+        int countTime = 100;
+
+        var arrBaker = new ArrayList<Baker>();
+        for (int i = 0; i < 2; i++) {
+            Baker a = new Baker(countTime, queueOrder, queueWarehouse);
+            a.start();
+            arrBaker.add(a);
+        }
+
+        Thread.sleep(150);
+        for (Baker a : arrBaker) {
+            a.interrupt();
+        }
+
+        for (Baker a : arrBaker) {
+            a.join();
+        }
+
+        assertEquals(queueOrder.size(), 2);
+        assertEquals(queueWarehouse.size(), 2);
+    }
+
+
 
 }
